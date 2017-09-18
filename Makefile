df1858e3f4e82a481f3bb6706663c33c0f3ae4bc
Jorge Pino <jpino@zeptocode.com>
2017-09-19 00:52:42 +0200

    Haloo

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ tmux:
 	ln -fs `pwd`/tmux/tmux.conf ~/.tmux.conf
 
 i3:
-	ln -fs `pwd`/i3 ~/.i3
+	ln -fs `pwd`/i3 ~/.config/i3
 
 urxvt:
 	echo "source `pwd`/urxvt/base16-eighties.dark.sh" >> ~/.bashrc
--- install-packages.sh
+++ install-packages.sh
@@ -2,6 +2,8 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
+add-apt-repository https://launchpad.net/~neovim-ppa/+archive/ubuntu/unstable
+
 
 # Update and upgrade packages
 apt update && apt upgrade
@@ -9,19 +11,21 @@ apt update && apt upgrade
 
 # List of new packages to be installed in no particular order
 PACKAGES="
+i3
+i3blocks
+fonts-font-awesome
+git
+neovim
+xsel
+
 libxml2-dev
 libxslt1-dev
 zlib1g-dev
 libffi-dev
-vim
 tmux
-git
 google-chrome-stable
 htop
-i3
-i3blocks
 xbacklight
-fonts-font-awesome
 mumble
 pavucontrol
 vlc
