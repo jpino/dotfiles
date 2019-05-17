46faca6362fa04e0d17f276db35061727e333c63
Jorge Pino <jpino@zeptocode.com>
2019-05-17 23:31:18 +0200

    Yes.

--- bash/base16.sh
+++ bash/base16.sh
@@ -6,9 +6,9 @@ if [[ -x "$SCRIPT_PATH" ]]
 then
     eval $SCRIPT_PATH
     echo "colorscheme base16-$1" > $HOME/.config/colors.vim
-    echo "let g:lightline = { 'colorscheme': 'base16_$1' }" > $HOME/.config/colors.lightline.vim
     echo "source $SCRIPT_PATH" > $HOME/.config/colors.shell
-    cat ~/.config/base16-i3/colors/base16-$1.config ~/dotfiles/i3/base > ~/.config/i3/config
+    cp  ~/.config/base16-i3/colors/base16-$1.config $HOME/.config/colors.i3
+    cat $HOME/.config/colors.i3 ~/dotfiles/i3/base > ~/dotfiles/i3/config
     i3-msg reload
 else
     echo "ERROR: File $SCRIPT_PATH is missing or not executable."
