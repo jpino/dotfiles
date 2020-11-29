20d323ceaa5274b6272967f481730fcdd015b76f
Jorge Pino <jpino@zeptocode.com>
2020-11-29 02:27:32 +0100

    Cleanup

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -2,17 +2,14 @@
 
 BAT0=$(cat /sys/class/power_supply/BAT0/capacity)
 BAT1=$(cat /sys/class/power_supply/BAT1/capacity)
-
 AC=$(cat /sys/class/power_supply/AC/online)
 
 if [[ $AC == "1" ]]; then
-    ACSTATUS="Connected to AC"
+    ACSTATUS="Charging"
 else
-    ACSTATUS="Not connected to AC"
+    ACSTATUS="Not charging"
 fi
 
-MESSAGE="Battery 0: $BAT0%
-Battery 1: $BAT1%
-$ACSTATUS"
+TOTAL=$((BAT0+$BAT1))
+notify-send -t 3000 "Battery: $TOTAL/200. $ACSTATUS"
 
-notify-send "Energy status" "$MESSAGE"
